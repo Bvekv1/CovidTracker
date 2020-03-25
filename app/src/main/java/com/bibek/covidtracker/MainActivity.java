5a408e12cd1a6cdb313e61cadbc36b0f0acf9f26
bvekv1 <bvekp1@gmail.com>
2020-03-25 15:29:20 +0545

    Initial commit

---
+++ app/src/main/java/com/bibek/covidtracker/MainActivity.java
@@ -0,0 +1,23 @@
+package com.bibek.covidtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+  private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.bing.com/covid");
+    }
+}
